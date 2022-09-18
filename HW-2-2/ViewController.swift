802d96b74106ec85864158d7fe9322095d73f988
Frysed <frysed@gmail.com>
2022-09-18 13:38:02 +0300

    change default alpha state for red color add button logic

--- HW-2-2/ViewController.swift
+++ HW-2-2/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var trafficLightColorRed: UIView!
     @IBOutlet var trafficLightColorYellow: UIView!
     @IBOutlet var trafficLightColorGreen: UIView!
@@ -20,14 +20,26 @@ class ViewController: UIViewController {
         trafficLightColorYellow.layer.cornerRadius = 65
         trafficLightColorGreen.layer.cornerRadius = 65
         trafficLightButton.layer.cornerRadius = 10
-        trafficLightColorYellow.alpha = 0.3
-        trafficLightColorGreen.alpha = 0.3
-        
     }
-
+    
     @IBAction func trafficLightButtonDidTapped() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        trafficLightSwitchColor()
     }
     
-    
+    private func trafficLightSwitchColor() {
+        if trafficLightColorRed.alpha == 1 {
+            trafficLightColorRed.alpha = 0.3
+            trafficLightColorYellow.alpha = 1
+        } else if trafficLightColorYellow.alpha == 1 {
+            trafficLightColorYellow.alpha = 0.3
+            trafficLightColorGreen.alpha = 1
+        } else {
+            trafficLightColorRed.alpha = 1
+            trafficLightColorYellow.alpha = 0.3
+            trafficLightColorGreen.alpha = 0.3
+        }
+    }
 }
+    
 
